0ba70b85e20999c74f02d231af8d8a99a9602415
rbing <rene@bing-in-sayn.de>
2024-09-04 22:03:52 +0200

    Add cmake build based on PR from dirkcgrunwald

--- win32/ITMDrvr/PointToPointMode.h
+++ win32/ITMDrvr/PointToPointMode.h
@@ -7,5 +7,9 @@ int ParseP2PInputFile(const char* in_file, P2PParams *p2p_params);
 int ValidateP2PInputs(P2PParams* p2p_params);
 int CallP2PMode(DrvrParams* params, P2PParams* p2p_params, IntermediateValues* inter_vals, vector<double>* A__db, long* warnings);
 void WriteP2PInputs(FILE* fp, P2PParams* p2p_params);
+#ifdef _WIN32
 int LoadP2PFunctions(HINSTANCE hLib);
+#else
+int LoadP2PFunctions(void *);
+#endif
 void ParseTerrainFile(const char* terrain_file, vector<double>* pfl);
